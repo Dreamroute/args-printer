c2b51040efbd1d4ce6dc81e1726581121766a56c
wangdehai <wangdehai@yzw.cn>
2023-05-13 21:55:20 +0800

    test: 增加controller单元测试

--- args-printer-sample/src/main/java/com/github/dreamroute/args/printer/smaple/controller/UserController.java
+++ args-printer-sample/src/main/java/com/github/dreamroute/args/printer/smaple/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.github.dreamroute.args.printer.smaple.controller;
 
+import cn.hutool.json.JSONUtil;
 import com.github.dreamroute.args.printer.smaple.domain.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author w.dehi.2021-09-28
  */
+@Slf4j
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
     @PostMapping("/selectById")
     public Object selectById(@RequestBody User user) {
-        System.err.println(user.getId());
+        log.info(JSONUtil.toJsonStr(user));
         return user;
     }
 }
---
+++ args-printer-sample/src/test/java/com/github/dreamroute/args/printer/smaple/controller/UserControllerTest.java
@@ -0,0 +1,32 @@
+package com.github.dreamroute.args.printer.smaple.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.github.dreamroute.args.printer.smaple.domain.User;
+import com.github.dreamroute.args.printer.spring.boot.starter.ArgsPrinterConfig;
+import com.github.dreamroute.common.util.test.Appender;
+import com.github.dreamroute.common.util.test.MvcTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@SpringBootTest
+class UserControllerTest extends MvcTest {
+
+    @Test
+    void selectByIdTest() throws Exception {
+        Appender appender = new Appender(ArgsPrinterConfig.class);
+        User user = new User();
+        user.setId(100L);
+        user.setName("w.dehai");
+        user.setPassword("123456");
+        mvc.perform(post("/user/selectById").content(JSON.toJSONString(user)).contentType(APPLICATION_JSON_VALUE))
+                .andDo(print());
+        boolean contains = appender.contains("参数: [{\"id\":100,\"name\":\"w.dehai\",\"password\":\"123456\"}]");
+        assertTrue(contains);
+    }
+
+}
